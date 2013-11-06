91e3d419d394944728703a2cef0240292ff92ab3
Noelia Rodriguez <alu0100724622@ull.edu.es>
2013-11-05 22:49:24 -1100

    añado gemfile

---
+++ Rakefile
@@ -0,0 +1,16 @@
+$:.unshift File.dirname(__FILE__) + 'lib'
+$:.unshift './lib', './spec'
+
+require 'rspec/core/rake_task'
+RSpec::Core::RakeTask.new
+task :default => :spec
+
+desc "Ejecutar las espectativas de la clase Matriz"
+task :local do
+  sh "rspec --format documentation -Ilib -Ispec spec/bdd_matriz.rb"
+end
+
+desc "Ejecutar los test de la clase Matriz"
+task :local do
+  sh "rspec --format documentation -Ilib -Ispec spec/tdd_matriz.rb"
+end
---
+++ spec/bdd_matriz.rb
@@ -0,0 +1,88 @@
+
+
+require "rspec"
+require "./matriz.rb"
+
+describe "metodo mostrar" do
+	before :each do
+		@matriz1 = Matriz.new(2,2)
+		@matriz2 = Matriz.new(3,3)
+	end
+
+	it "mostrando la matriz 1" do
+	
+		@matriz1.to_s(1,2,3,4).should eq([[1, 2], [3, 4]])
+	end
+
+	it "mostrado la matriz 2" do
+
+		@matriz2.to_s(1,2,3,4,5,6,7,8,9).should eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+	end
+
+end
+
+describe "operaciones aritmeticas" do
+
+	before :each do
+		@matriz1 = Matriz.new(2,2,1,2,3,4)
+		@matriz2 = Matriz.new(2,2,1,1,1,1)
+		@matriz3 = Matriz.new(2,3,1,1,1,2,2,2)
+		@matriz4 = Matriz.new(2,3,3,3,3,4,4,4)
+		@matriz5 = Matriz.new(3,3,1,1,1,2,2,2,3,3,3)
+		@matriz6 = Matriz.new(3,3,4,4,4,5,5,5,6,6,6)
+	end
+
+	it "mostrando la suma" do
+
+		@matriz1 + @matriz2 == Matriz.new(2,2,2,3,4,5)
+		@matriz2 + @matriz2 == Matriz.new(2,2,2,2,2,2)
+
+		@matriz3 + @matriz4 == Matriz.new(2,3,4,4,4,6,6,6)
+		@matriz3 + @matriz3 == Matriz.new(2,3,2,2,2,4,4,4)
+	
+	end
+
+
+	it "mostrando la resta" do
+
+		@matriz1 - @matriz2 == Matriz.new(2,2,0,1,2,3)
+		@matriz2 - @matriz2 == Matriz.new(2,2,0,0,0,0)
+
+		@matriz4 - @matriz3 == Matriz.new(2,3,2,2,2,2,2,2)
+		@matriz3 - @matriz3 == Matriz.new(2,3,0,0,0,0,0,0)
+	
+	end
+
+	it "mostrando la multiplicacion" do
+
+		@matriz1 * @matriz2 == Matriz.new(2,2,3,3,7,7)
+		@matriz2 * @matriz1 == Matriz.new(2,2,4,6,4,6)
+
+		@matriz5 * @matriz6 == Matriz.new(3,3,15,15,15,30,30,30,45,45,45)
+		@matriz6 * @matriz5 == Matriz.new(3,3,24,24,24,30,30,30,36,36,36)
+	
+	end
+
+end
+
+describe "operaciones logicas" do
+
+	before :each do
+		@matriz1 = Matriz.new(2,2)
+		@matriz2 = Matriz.new(3,3)
+	end
+
+	it "mayor" do
+		(@matriz2 > @matriz1).should be_true
+	end
+
+	it "menor" do
+		(@matriz1 < @matriz2).should be_true
+	end
+
+	it "igual" do
+		(@matriz1 == @matriz2).should be_false
+	end
+	
+end
